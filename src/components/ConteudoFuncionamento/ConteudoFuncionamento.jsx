4d6ab5c82b7dd249ed5527d8cb5efb8b5097166e
PalomaRodrigues33 <rm551321@fiap.com.br>
2023-11-12 21:44:47 -0300

    pequenas alterações scss

--- src/components/ConteudoFuncionamento/ConteudoFuncionamento.jsx
+++ src/components/ConteudoFuncionamento/ConteudoFuncionamento.jsx
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
+import './ConteudoFuncionamento.scss'
 
 import image6 from '../../img/image6.png';
 
--- src/components/ConteudoSobre/ConteudoSobre.jsx
+++ src/components/ConteudoSobre/ConteudoSobre.jsx
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
+import './ConteudoSobre.scss'
 
 import image5 from '../../img/image5.png';
 
